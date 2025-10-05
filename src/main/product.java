cb6648e2923e8f9ec44633b0ccb692d041f2cffd
devopseusr <archanapradeep282@gmail.com>
2025-10-05 20:30:46 +0530

    Create product.java

---
+++ src/main/product.java
@@ -0,0 +1,23 @@
+package com.shopsmart.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double price;
+    private int quantity;
+
+    public Product() {}
+
+    public Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    // getters and setters
+}
